d96e1b24dc4a1aeeccc24cf9a8904e9257b268af
Nathan Malnoury <nathan.malnoury@experquiz.com>
2021-08-12 11:55:06 +0200

    First working version
    Based on github API.
    Procedure as follow:
    - From github event json, get PR number, Head and target branches
    - From api, get commits in that PR
    - Using Git, switch to the pr_branch and cherry pick each commit retrieved earlier
    
    - POST-ing on github api, it either opens a PR if everything went OK, or an issue, with some info. If github API is unavailable, output the trace to the action stdout itself.

---
+++ helpers.py
@@ -0,0 +1,110 @@
+import os
+import subprocess
+import typing
+from subprocess import CalledProcessError
+
+import requests
+
+
+class GitException(Exception):
+    """Exception raised when using git command line from python."""
+
+    pass
+
+
+def git(*args):
+    try:
+        command_run = subprocess.run(["git", *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        if command_run.stdout is not None:
+            return command_run.stdout.decode()
+    except CalledProcessError as e:
+        output = e.stderr
+        try:
+            output = output.decode()
+        except:
+            pass
+        raise GitException(output)
+
+
+def _get_base_branch(event_dict: typing.Dict) -> str:
+    try:
+        return event_dict["pull_request"]["base"]["ref"]
+    except:
+        raise RuntimeError("pull_request.base.ref not found in GITHUB_EVENT_PATH")
+
+
+def _get_target_branch(event_dict: typing.Dict) -> str:
+    try:
+        return event_dict["pull_request"]["head"]["ref"]
+    except:
+        raise RuntimeError("pull_request.head.ref not found in GITHUB_EVENT_PATH")
+
+
+def _get_pr_number(event_dict: typing.Dict) -> int:
+    try:
+        return event_dict["pull_request"]["number"]
+    except:
+        raise RuntimeError("pull_request.number not found in GITHUB_EVENT_PATH")
+
+
+def git_setup(github_token):
+    repo = os.getenv("GITHUB_REPOSITORY")
+    actor = os.getenv("GITHUB_ACTOR")
+    git(
+        "remote",
+        "set-url",
+        "--push",
+        "origin",
+        f"https://{actor}:{github_token}@github.com/{repo}.git",
+    )
+    git("config", "user.email", "action@github.com")
+    git("config", "user.name", "github action")
+
+
+def github_api_headers(gh_token):
+    return {
+        "authorization": f"Bearer {gh_token}",
+        "content-type": f"application/json",
+        "accept": "application/vnd.github.v3+json",
+    }
+
+
+def _github_repo_url():
+    repo = os.getenv("GITHUB_REPOSITORY")
+    api_url = os.getenv("GITHUB_API_URL")
+    return f"{api_url}/repos/{repo}"
+
+
+def github_open_pull_request(title: str, body: str, head: str, base: str, gh_token: str):
+    headers = github_api_headers(gh_token=gh_token)
+    body = {
+        "head": head,
+        "base": base,
+        "title": title,
+        "body": body,
+    }
+
+    response = requests.post(url=f"{_github_repo_url()}/pulls", json=body, headers=headers)
+    response.raise_for_status()
+
+
+def github_open_issue(title: str, body: str, gh_token: str):
+    headers = github_api_headers(gh_token=gh_token)
+    body = {
+        "title": title,
+        "body": body,
+    }
+
+    response = requests.post(url=f"{_github_repo_url()}/issues", json=body, headers=headers)
+    response.raise_for_status()
+
+
+def github_get_commits_in_pr(pr_number: int, gh_token: str) -> typing.Any:
+    headers = github_api_headers(gh_token=gh_token)
+
+    response = requests.get(url=f"{_github_repo_url()}/pulls/{pr_number}/commits", headers=headers)
+    response.raise_for_status()
+    commits = []
+    for commit in response.json():
+        commits.append(commit["sha"])
+    return commits
--- main.py
+++ main.py
@@ -1,124 +1,57 @@
-"""
-To consider:
- - Error scenario -> open issue ? Open PR with conflicts
- - Branch deletion of the duplicated target branch
-"""
 import argparse
 import json
 import os
-import subprocess
-from pathlib import Path
-
 import typing
-import requests
+import traceback
+from datetime import datetime
 
-repo_path = Path("../cherry-pick-tests")
-
-
-def git(*args):
-    command_run = subprocess.run(["git", *args], stdout=subprocess.PIPE, check=True)
-    if command_run.stdout is not None:
-        return command_run.stdout.decode()
-
-
-def get_commits_in_branch(
-    merge_commit_sha: str, target_branch: typing.Optional[str]
-) -> typing.List[str]:
-    if not target_branch:
-        target_branch = "."  # that way compare `base_branch` to current branch.
-    list_commits_as_str = git(
-        "cherry", merge_commit_sha + "~1", f"origin/{target_branch}"
-    )
-
-    # return multiple line `[+/-] hash`. If "+" needs cherry-pick, else has an equivalent already.
-    lines = list_commits_as_str.split("\n")
-    commit_split: typing.List[typing.List[str, str]] = [
-        line_commit.split() for line_commit in lines if line_commit != ""
-    ]
-    commit_hashes = [line[1] for line in commit_split if line[0] == "+"]
-
-    return commit_hashes
+from helpers import (
+    _get_base_branch,
+    _get_target_branch,
+    _get_pr_number,
+    git,
+    github_get_commits_in_pr,
+    github_open_pull_request,
+    git_setup,
+    github_open_issue,
+)
 
 
 def backport_commits(commits: typing.List[str], initial_name: str, to_branch: str):
     """
     Backport a list of commit on a *new* branch starting from to_branch.
     """
-    new_branch = f"backport-{initial_name}-{to_branch}"
+
+    new_branch = f"backport-{initial_name[:15]}-{datetime.utcnow().strftime('%m%d%y')}-{to_branch}"
     git("switch", "-c", new_branch, "origin/" + to_branch)
     print(f"Switched to future branch: {new_branch}.")
     try:
-        for commit_hash in commits[::-1]:  # status returns newer first.
+        for commit_hash in commits:
             git("cherry-pick", commit_hash)
     except:
         print("An error occurred while cherry-picking.")
+        raise RuntimeError("Could not cherry pick at least one commit automatically.")
+
     git("push", "-u", "origin", new_branch)
-
-
-def _get_merge_commit_sha(event_dict: typing.Dict) -> str:
-    try:
-        return event_dict["pull_request"]["merge_commit_sha"]
-    except:
-        raise RuntimeError(
-            "pull_request.merge_commit_sha not found in GITHUB_EVENT_PATH"
-        )
-
-
-def _get_base_branch(event_dict: typing.Dict) -> str:
-    try:
-        return event_dict["pull_request"]["base"]["ref"]
-    except:
-        raise RuntimeError("pull_request.base.ref not found in GITHUB_EVENT_PATH")
-
-
-def _get_target_branch(event_dict: typing.Dict) -> str:
-    try:
-        return event_dict["pull_request"]["head"]["ref"]
-    except:
-        raise RuntimeError("pull_request.head.ref not found in GITHUB_EVENT_PATH")
+    return new_branch
 
 
 def entrypoint(event_dict, pr_branch, gh_token):
-    merge_sha = _get_merge_commit_sha(event_dict)
     base_branch = _get_base_branch(event_dict)
-    target_branch = _get_target_branch(event_dict)
-    git("checkout", base_branch)
-    commits_to_backport = get_commits_in_branch(
-        merge_commit_sha=merge_sha, target_branch=target_branch
-    )
+    pr_number = _get_pr_number(event_dict)
+
+    commits_to_backport = github_get_commits_in_pr(pr_number=pr_number, gh_token=gh_token)
+
     print(f"found {len(commits_to_backport)} commits to backport.")
-    backport_commits(commits_to_backport, merge_sha, pr_branch)
 
-
-def git_setup(github_token):
-    repo = os.getenv("GITHUB_REPOSITORY")
-    actor = os.getenv("GITHUB_ACTOR")
-    git(
-        "remote",
-        "set-url",
-        "--push",
-        "origin",
-        f"https://{actor}:{github_token}@github.com/{repo}.git",
+    new_branch = backport_commits(commits_to_backport, base_branch, pr_branch)
+    github_open_pull_request(
+        title=f"chore: backport #{pr_number} into {pr_branch}",
+        head=new_branch,
+        base=pr_branch,
+        body=f"An automated backport for #{pr_number}.",
+        gh_token=gh_token,
     )
-    git("config", "user.email", "action@github.com")
-    git("config", "user.name", "github action")
-
-
-def get_commits_to_backport(gh_token, commits_url):
-
-    headers = {
-        "authorization": f"Bearer { gh_token }",
-        "content-type": f"application/json",
-        "accept": "application/vnd.github.v3+json",
-    }
-    req = requests.get(url=commits_url, headers=headers)
-    json = req.json()
-    commits = []
-    for entry in json:
-        sha = entry.get("sha", "")
-        print(sha, entry)
-        commits.append(sha)
-    return commits
 
 
 if __name__ == "__main__":
@@ -126,14 +59,49 @@ if __name__ == "__main__":
     parser.add_argument("pr_branch", type=str)
     parser.add_argument("github_token", type=str)
     github_event_path = os.getenv("GITHUB_EVENT_PATH")
+
     with open(github_event_path, "r") as f:
         github_event = json.load(f)
 
     args = parser.parse_args()
     git_setup(args.github_token)
+    try:
+        entrypoint(
+            event_dict=github_event,
+            pr_branch=args.pr_branch,
+            gh_token=args.github_token,
+        )
+    except Exception as main_exception:
+        main_traceback = traceback.format_exc()
+        traceback_formatted_for_body = f"\n```python\n{main_traceback}```"
+        try:
+            pr_num = _get_pr_number(event_dict=github_event)
+            title = f"Could not automatically backport #{pr_num}"
+            body = f"Exception occurred when trying to cherry-pick PR #{pr_num}.\nPlease cherry-pick it manually."
+            body += traceback_formatted_for_body
+            github_open_issue(title, body, gh_token=args.github_token)
+            exit(1)
+        except Exception:  # could not get pr number ; fallback on next try/except
+            pass
+        try:
+            tar_branch = _get_target_branch(event_dict=github_event)
+            title = f"Could not automatically backport branch {tar_branch}"
+            body = f"Exception occurred when trying to cherry-pick PR commits of `{tar_branch}`.\nPlease cherry-pick it manually."
+            body += traceback_formatted_for_body
 
-    entrypoint(
-        event_dict=github_event,
-        pr_branch=args.pr_branch,
-        gh_token=args.github_token,
-    )
+            github_open_issue(title, body, gh_token=args.github_token)
+            exit(1)
+        except Exception:  # could not get target branch; fallback on next try/except
+            pass
+        try:
+            title = f"Automatic Backport failed"
+            body = f"Exception occurred when trying to backport a branch.\nCheck `actions` tab to see more."
+            body += traceback_formatted_for_body
+            github_open_issue(title, body, gh_token=args.github_token)
+            exit(1)
+        except Exception:  # could not create a github issue;
+            print("Several Exceptions occurred:")
+            print(traceback)
+            print("--" * 8)
+            print(traceback.format_exc())
+            exit(1)
